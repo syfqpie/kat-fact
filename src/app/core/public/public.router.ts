154daa85961726e3b65a92baa98052e0c2b319ba
Syafiq Basri <syafiq@talentx.ai>
2022-03-13 01:43:28 +0800

    Changed /home to /; Changed navbar layout for mobile

--- src/app/core/public/public.router.ts
+++ src/app/core/public/public.router.ts
@@ -4,11 +4,11 @@ import { HomeComponent } from './home/home.component';
 
 export const PublicRoutes: Routes = [
     {
-        path: 'about',
-        component: AboutComponent
+        path: '',
+        component: HomeComponent
     },
     {
-        path: 'home',
-        component: HomeComponent
+        path: 'about',
+        component: AboutComponent
     }
 ];
